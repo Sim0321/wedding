53d5ca5b030205aaa4ea234ae4dbd8da27a70610
Sim <0doogadooga@gmail.com>
2024-05-15 00:17:54 +0900

    #3 Heading date-fns 이용해 날짜, 요일 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import classNames from 'classnames/bind'
-import styles from './App.module.scss'
 import { useEffect, useState } from 'react'
+
+import styles from './App.module.scss'
+
 import FullScreenMessage from '@shared/FullScreenMessage'
+
 import Heading from './components/sections/Heading'
 import Video from './components/sections/Video'
+
+import { Wedding } from '@models/wedding'
+
 const cx = classNames.bind(styles)
 
 function App() {
-  const [wedding, setWedding] = useState(null)
+  const [wedding, setWedding] = useState<Wedding | null>(null)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
 
@@ -41,9 +47,13 @@ function App() {
     return <FullScreenMessage type="error" />
   }
 
+  if (wedding === null) return null
+
+  const { date } = wedding
+
   return (
     <div className={cx('container')}>
-      <Heading />
+      <Heading date={date} />
       <Video />
       {JSON.stringify(wedding)}
     </div>
--- src/components/sections/Heading.tsx
+++ src/components/sections/Heading.tsx
@@ -1,14 +1,27 @@
-import Section from '@shared/Section'
+import { parseISO, format, getDay } from 'date-fns'
 import classNames from 'classnames/bind'
 import styles from './Heading.module.scss'
 
+import Section from '@shared/Section'
+
 const cx = classNames.bind(styles)
 
-function Heading() {
+const DAYS = [
+  '일요일',
+  '월요일',
+  '화요일',
+  '수요일',
+  '목요일',
+  '금요일',
+  '토요일',
+]
+
+function Heading({ date }: { date: string }) {
+  const weddingDate = parseISO(date)
   return (
     <Section className={cx('container')}>
-      <div className={cx('txt-date')}>24.05.14</div>
-      <div className={cx('txt-day')}>TUESDAY</div>
+      <div className={cx('txt-date')}>{format(weddingDate, 'yy.MM.dd')}</div>
+      <div className={cx('txt-day')}>{DAYS[getDay(weddingDate)]}</div>
     </Section>
   )
 }
