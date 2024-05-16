4225c1f7f4eeeaba7fa7e2e0a362a0477374f8db
Sim <0doogadooga@gmail.com>
2024-05-17 01:39:31 +0900

    #8 전화걸기 완료, 분기 미완료

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -5,15 +5,97 @@ import Section from '@shared/Section'
 
 import React from 'react'
 import Accordion from '@shared/Accordion'
+import { Person, Wedding } from '@/models/wedding'
 
 const cx = classNames.bind(styles)
 
-const Contact = () => {
+const Contact = ({
+  groom,
+  bride,
+}: {
+  groom: Wedding['groom']
+  bride: Wedding['bride']
+}) => {
   return (
     <Section title="연락처 및 마음 전하실 곳">
-      <Accordion label="신랑측">sdfsdfsd</Accordion>
-      <Accordion label="신부측"> 신부측 신부측 신부측</Accordion>
+      <Accordion label="신랑측">
+        <ContactInfo
+          name={groom.name}
+          account={groom.account}
+          phoneNumber={groom.phoneNumber}
+        />
+        <ContactInfo
+          name={groom.parents[0].name}
+          account={groom.parents[0].account}
+          phoneNumber={groom.parents[0].phoneNumber}
+        />
+        <ContactInfo
+          name={groom.parents[1].name}
+          account={groom.parents[1].account}
+          phoneNumber={groom.parents[1].phoneNumber}
+        />
+      </Accordion>
+      <Accordion label="신부측">
+        <ContactInfo
+          name={bride.name}
+          account={bride.account}
+          phoneNumber={bride.phoneNumber}
+        />
+        <ContactInfo
+          name={bride.parents[0].name}
+          account={bride.parents[0].account}
+          phoneNumber={bride.parents[0].phoneNumber}
+        />
+        <ContactInfo
+          name={bride.parents[1].name}
+          account={bride.parents[1].account}
+          phoneNumber={bride.parents[1].phoneNumber}
+        />
+      </Accordion>
     </Section>
+  )
+}
+
+function ContactInfo({ name, account, phoneNumber }: Person) {
+  // 모바일인지 웹인지 구분해서 팝업창 띄워주는 함수
+
+  return (
+    <div className={cx('wrap-contact')}>
+      <div className={cx('wrap-contact-info')}>
+        {/* 정보 표현 */}
+        <span>{`${account.bankName} | ${account.accountNumber}`}</span>
+        <span>{name}</span>
+      </div>
+      <ul className={cx('wrap-buttons')}>
+        {/* 버튼들 */}
+        <li>
+          <a href={`$tell:${phoneNumber}`} className={cx('button')}>
+            전화
+          </a>
+          {/* <div
+            className={cx('button')}
+            onClick={() => (document.location.href = 'tel:전화번호')}
+          >
+            전화
+          </div> */}
+        </li>
+        <li>
+          <button className={cx('button')}>복사</button>
+        </li>
+        {account.kakaopayLink !== null ? (
+          <li>
+            <a
+              href={account.kakaopayLink}
+              target="_blank"
+              rel="noreferrer"
+              className={cx('button')}
+            >
+              송금
+            </a>
+          </li>
+        ) : null}
+      </ul>
+    </div>
   )
 }
 
--- src/models/wedding.ts
+++ src/models/wedding.ts
@@ -35,4 +35,5 @@ export interface Person {
 export interface Account {
   bankName: string
   accountNumber: string
+  kakaopayLink?: string
 }
