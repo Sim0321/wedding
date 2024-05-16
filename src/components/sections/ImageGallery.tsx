58c33d0f524e2948281fa48331c3095b8e7d3c74
Sim <0doogadooga@gmail.com>
2024-05-16 15:33:35 +0900

    #4 swiper를 이용해 캐러셀 구현

--- src/components/sections/ImageGallery.tsx
+++ src/components/sections/ImageGallery.tsx
@@ -1,20 +1,45 @@
 import classNames from 'classnames/bind'
 import Section from '@shared/Section'
 import styles from './ImageGallery.module.scss'
+import ImageViewer from '../imageViewer/ImageViewer'
+import { useState } from 'react'
 
 const cx = classNames.bind(styles)
 
 function ImageGallery({ images }: { images: string[] }) {
+  const [selectedIndex, setSelectedIndex] = useState(-1)
+
+  const open = selectedIndex > -1
+
+  const handleselectedImage = (idx: number) => {
+    setSelectedIndex(idx)
+  }
+
+  const handleClose = () => {
+    setSelectedIndex(-1)
+  }
   return (
-    <Section title="사진첩">
-      <ul className={cx('wrap-images')}>
-        {images.map((src, idx) => (
-          <li key={idx} className={cx('wrap-image')}>
-            <img src={src} alt="사진첩 이미지" />
-          </li>
-        ))}
-      </ul>
-    </Section>
+    <>
+      <Section title="사진첩">
+        <ul className={cx('wrap-images')}>
+          {images.map((src, idx) => (
+            <li
+              key={idx}
+              className={cx('wrap-image')}
+              onClick={() => handleselectedImage(idx)}
+            >
+              <img src={src} alt="사진첩 이미지" />
+            </li>
+          ))}
+        </ul>
+      </Section>
+      <ImageViewer
+        images={images}
+        open={open}
+        selectedIndex={selectedIndex}
+        onClose={handleClose}
+      />
+    </>
   )
 }
 
